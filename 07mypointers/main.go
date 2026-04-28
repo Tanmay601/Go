73f1f3dcf8226db81215fe6d03b271f9b98e0201
TANMAY RAJPOOT <tanmayrajpoot07@gmail.com>
2026-04-29 00:23:19 +0530

    memory management is taken care of here easily

--- 07mypointers/main.go
+++ 07mypointers/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt" 
+import "fmt"
 
+func main() {
+	fmt.Println("Pointers")
 
-func main(){
-	fmt.Println"Pointers"
+	//var ptr *int
+	//fmt.Println("value of the pointer is ", ptr)
+	//default value of  apointer is nil obviooo
+
+	myNumber := 23
+
+	var ptr = &myNumber //a pointer which is referencing to some memory address using & sign
+
+	fmt.Println("value of the pointer is ", ptr)
+	fmt.Println("value of the pointer is ", *ptr)
+
+	*ptr = *ptr + 2
+	fmt.Println("New Value is:", myNumber)
+
 }
