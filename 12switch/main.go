56e00ef43c4bb2119637160cfc065e070dddfafa
TANMAY RAJPOOT <tanmayrajpoot07@gmail.com>
2026-05-03 00:05:56 +0530

    allg

--- 12switch/main.go
+++ 12switch/main.go
@@ -12,4 +12,11 @@ func main() {
 	diceNumber := rand.Intn(6) + 1 // generates a number between 1 and 6
 	fmt.Println("You rolled a", diceNumber)
 
+	switch diceNumber {
+	case 1:
+		fmt.Println("You rolled a one!")
+	case 2:
+		fmt.Println("You rolled a two!")
+	}
+
 }
