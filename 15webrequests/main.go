299ae650bb6d17b5f2c1657955a09a309a9c944c
Tanmay <tanmayrajpoot07@gmail.com>
2026-05-25 00:46:22 +0530

    web req

---
+++ 15webrequests/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://lco.dev"
+
+func main() {
+	fmt.Println("web req in GO")
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Response is of type:", response)
+
+	defer response.Body.Close() //callers responsibility to close the connection
+
+	databytes, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(databytes)
+	fmt.Println("Content:", content)
+}
