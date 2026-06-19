6d4304665fda99e595ed73f9328a07a314d7c082
Tanmay <tanmayrajpoot07@gmail.com>
2026-06-19 23:10:25 +0530

    schemes

--- 16urlshandles/main.go
+++ 16urlshandles/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
 
 const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=avhsa234"
 
 func main() {
 	fmt.Println("URLs in golang")
 	fmt.Println(myurl)
+
+	//parsing
+	result, _ := url.Parse(myurl)
+
+	fmt.Println(result.Scheme)
 
 }
