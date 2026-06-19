1d52127eb452de5df1819b1518ee2b86b1850dd6
Tanmay <tanmayrajpoot07@gmail.com>
2026-06-19 23:16:56 +0530

    construct url

--- 16urlshandles/main.go
+++ 16urlshandles/main.go
@@ -15,5 +15,22 @@ func main() {
 	result, _ := url.Parse(myurl)
 
 	fmt.Println(result.Scheme)
+	fmt.Println(result.Host)
+	fmt.Println(result.Path)
+	fmt.Println(result.Port())
+
+	qparams := result.Query()
+
+	fmt.Printf("The type of query params is: %T\n", qparams)
+
+	partsOfUrl := &url.URL{
+
+		Scheme:   "https",
+		Host:     "lco.dev",
+		Path:     "/tutcss",
+		RawQuery: "user=TR",
+	}
+	anotherURL := partsOfUrl.String()
+	fmt.Println("Another URL is:", anotherURL)
 
 }
