5467e7b2abeccdcabb43f159e501ca6e79be199a
Tanmay <tanmayrajpoot07@gmail.com>
2026-06-19 23:06:18 +0530

    URLs

---
+++ 16urlshandles/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=avhsa234"
+
+func main() {
+	fmt.Println("URLs in golang")
+	fmt.Println(myurl)
+
+}
